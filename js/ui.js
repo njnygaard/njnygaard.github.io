1d8ea0a8854d292c8a0a41fdf1cbab26dd13e8e2
Nikhil Nygaard <Nikhil Nygaard>
2017-10-05 12:32:49 -0700

    SSH Review

--- js/ui.js
+++ js/ui.js
@@ -43,12 +43,4 @@
         }
     };
 
-    // Shrink images to the width of the article.
-    var elements = document.getElementsByTagName("img");
-    for (var i=0; i<elements.length; i++)
-    {
-      var el = elements[i];      
-      el.setAttribute( "class", el.getAttribute("class") + " content" );
-    }
-
 }(this, this.document));
